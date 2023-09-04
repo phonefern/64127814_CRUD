65c5f5c2fb87fd3b89e6308d91ce4c00fecb331a
phonefern <110513325+phonefern@users.noreply.github.com>
2023-09-04 18:31:21 +0700

    18.31/4/9

--- flutter_application_1/lib/Screen/UserInfo.dart
+++ flutter_application_1/lib/Screen/UserInfo.dart
@@ -5,24 +5,53 @@ class UserInfo extends StatelessWidget {
   const UserInfo({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Users user = ModalRoute.of(context)!.settings.arguments as Users;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("User Info"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child:Card(
+Widget build(BuildContext context) {
+  Users user = ModalRoute.of(context)!.settings.arguments as Users;
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text("User Info"),
+    ),
+    body: Container(
+      margin: const EdgeInsets.all(10),
+      child: Card(
         child: ListView(
           children: [
-            ListTile(title: Text("Full Name"), subtitle: Text("${user.fullname}"),),
-            ListTile(title: Text("Email"),subtitle: Text("${user.email}"),),
-            ListTile(title: Text("Gender"),subtitle: Text("${user.gender}"),),
+            ListTile(
+              title: const Text("Name"),
+              subtitle: Text(
+                ("${user.fullname}"),
+                style: const TextStyle(
+                  color: Colors.black, // สีข้อความ
+                  fontSize: 16, // ขนาดตัวอักษร
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text("Email"),
+              subtitle: Text(
+                ("${user.email}"),
+                style: const TextStyle(
+                  color: Colors.black, // สีข้อความ
+                  fontSize: 16, // ขนาดตัวอักษร
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text("Gender"),
+              subtitle: Text(
+                ("${user.gender}"),
+                style: const TextStyle(
+                  color: Colors.black, // สีข้อความ
+                  fontSize: 16, // ขนาดตัวอักษร
+                ),
+              ),
+            ),
           ],
         ),
       ),
-      ),
-    );
-  }
+    ),
+  );
 }
+
+  }
+
--- flutter_application_1/lib/Screen/welcome.dart
+++ flutter_application_1/lib/Screen/welcome.dart
@@ -6,10 +6,12 @@ import 'package:flutter_application_1/main.dart';
 import 'package:flutter_application_1/models/Users.dart';
 import 'package:flutter_application_1/models/config.dart';
 import 'package:http/http.dart' as http;
+import 'package:dotted_line/dotted_line.dart';
 
 class welcome extends StatefulWidget {
   static String routeName = "/";
   const welcome({super.key});
+  
 
   @override
   State<welcome> createState() => _welcomeState();
@@ -38,29 +40,72 @@ class _welcomeState extends State<welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome"),
-        ),
-        drawer: Sidemenu(),
-        body: mainbody,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            String result = await Navigator.push(
-              context, MaterialPageRoute(builder: (context) => UserForm()));
-          if (result == "refresh") {
-            getUsers();
-          }
-          },
-          child: Icon(Icons.person_add_alt_1),
-        ));
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 71, 177, 238),
+        title: const Text("Patient",
+            style: TextStyle(
+                fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center), // ข้อความอยู่ตรงกลาง
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person_add_alt_1),
+            onPressed: () async {
+              String result = await Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const UserForm()));
+              if (result == "refresh") {
+                getUsers();
+              }
+            },
+          ),
+        ],
+      ),
+      drawer: const Sidemenu(),
+      body: mainbody,
+    );
   }
-
 
   Widget showUsers() {
     return ListView.builder(
-      itemCount: _userList.length,
+      itemCount: _userList.length + 1, // เพิ่ม 1 เพื่อให้มีข้อความด้านบนสุด
       itemBuilder: (context, index) {
-        Users user = _userList[index];
+        if (index == 0) {
+          // สร้างข้อความด้านบนสุด
+          return const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "ID",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      "All Patient",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(25, 10, 25, 1),
+                  child: Divider(
+                    thickness: 6,
+                    color: Color.fromARGB(255, 87, 154, 209),
+                  ),
+                )
+              ],
+            ),
+          );
+        }
+        Users user = _userList[index - 1];
         return Dismissible(
           key: UniqueKey(),
           direction: DismissDirection.endToStart,
@@ -69,38 +114,73 @@ class _welcomeState extends State<welcome> {
           },
           background: Container(
             color: Colors.red,
-            margin: EdgeInsets.symmetric(horizontal: 15),
+            margin: const EdgeInsets.symmetric(horizontal: 15),
             alignment: Alignment.centerRight,
-            child: Icon(Icons.delete, color: Colors.white),
-            
+            child: const Icon(Icons.delete, color: Colors.white),
           ),
-          
-          
-          child: Card(
-            child: ListTile(
-              title: Text("${user.fullname}"),
-              subtitle: Text("HN: ${user.hn}"),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const UserInfo(),
-                        settings: RouteSettings(arguments: user)));
-              },
-              trailing: IconButton(
-                onPressed: () async {
-                  String result = await Navigator.push(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Padding(
+              //   padding: const EdgeInsets.fromLTRB(45, 10, 1, 10),
+              //   child: Text(
+              //     '${user.id}',
+              //     style: const TextStyle(
+              //       fontWeight: FontWeight.w900,
+              //       fontStyle: FontStyle.normal,
+              //       fontSize: 15,
+              //     ),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(
+                    40, 20, 15, 20), // ปรับแต่ง Padding ตามความต้องการ
+                child: ListTile(
+                  title: Text(
+                    '0${user.id}      ${user.fullname}', // เชื่อม id และ fullname ในบรรทัดเดียวกัน
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 17,
+                    ),
+                  ),
+                  subtitle: Text("HN:     ${user.hn}"),
+                  onTap: () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => UserForm(),
-                          settings: RouteSettings(arguments: user)));
-                  if (result == "refresh") {
-                    getUsers();
-                  }
-                },
-                icon: Icon(Icons.edit),
+                        builder: (context) => const UserInfo(),
+                        settings: RouteSettings(arguments: user),
+                      ),
+                    );
+                  },
+                  trailing: IconButton(
+                    onPressed: () async {
+                      String result = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const UserForm(),
+                          settings: RouteSettings(arguments: user),
+                        ),
+                      );
+                      if (result == "refresh") {
+                        getUsers();
+                      }
+                    },
+                    icon: const Icon(Icons.edit),
+                  ),
+                ),
               ),
-            ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(25, 1, 25, 1),
+                child: DottedLine(
+                  dashColor: Color.fromARGB(255, 0, 149, 194), // สีของเส้นประ
+                  dashGapLength: 5.0, // ความยาวระหว่างเส้นประแท็บ
+                  dashLength: 15.0, // ความยาวของแต่ละเส้นประ
+                  lineThickness: 3.0, // ความหนาของเส้นประ
+                ),
+              ),
+            ],
           ),
         );
       },
@@ -136,46 +216,63 @@ class Sidemenu extends StatelessWidget {
     }
 
     return Drawer(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        UserAccountsDrawerHeader(
-          accountName: Text(accountName),
-          accountEmail: Text(accountEmail),
-          currentAccountPicture: CircleAvatar(
-            backgroundImage: AssetImage(accountpic),
-            backgroundColor: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            height: 250, // ส่วนสูงของ UserAccountsDrawerHeader
+            child: Align(
+              alignment: Alignment
+                  .center, // จัดวาง UserAccountsDrawerHeader อยู่ตรงกลาง
+              child: UserAccountsDrawerHeader(
+                accountName: Text(accountName),
+                accountEmail: Text(accountEmail),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage(accountpic),
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            ),
           ),
-        ),
-        ListTile(
-          leading: Icon(Icons.home_max_sharp),
-          title: Text("Home"),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return welcome();
-            }));
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.login_sharp),
-          title: Text("Login"),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return LoginScreen();
-            }));
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.logout_sharp),
-          title: Text("logout"),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return start();
-            }));
-          },
-        )
-      ],
-    ));
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.home_max_sharp),
+                  title: const Text("Home"),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const welcome();
+                    }));
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.login_sharp),
+                  title: const Text("Login"),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const LoginScreen();
+                    }));
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.logout_sharp),
+                  title: const Text("Logout"),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const start();
+                    }));
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
