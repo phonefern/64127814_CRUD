6f180141a9200daf35afe4780b12bd543a90be63
phonefern <110513325+phonefern@users.noreply.github.com>
2023-09-06 16:41:41 +0700

    Update config.dart

--- flutter_application_1/lib/models/config.dart
+++ flutter_application_1/lib/models/config.dart
@@ -2,7 +2,7 @@
 import 'package:flutter_application_1/models/Users.dart';
 
 class Configure{
-  static const server = "192.168.1.106:3000";
+  static const server = "192.168.7.227:3000";
   static Users login = Users();
   static List<String> gender = [
     "None",
